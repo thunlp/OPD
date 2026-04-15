283e48c73c7fc2a6c599556e117d0c0eafef54b2
Turingzero0 <2206881609@qq.com>
2026-04-15 09:21:05 +0800

    fix: disable thinking in chat template kwargs for prompt processing

--- grpo.sh
+++ grpo.sh
@@ -158,6 +158,7 @@ python3 -m verl.trainer.main_ppo \
     data.filter_overlong_prompts=True \
     data.truncation='error' \
     data.return_raw_chat=True \
+    +data.apply_chat_template_kwargs.enable_thinking=False \
     actor_rollout_ref.model.path=$ACTOR_MODEL_PATH \
     actor_rollout_ref.model.use_remove_padding=True \
     actor_rollout_ref.model.enable_activation_offload=True \
